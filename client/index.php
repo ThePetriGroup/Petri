bd78256a43c4feb805491f5534e0035380f642b6
Kaliom <Kaliom@users.noreply.github.com>
2015-07-03 17:00:01 +0200

    Create index.php
    I'm sure we need this :D

---
+++ client/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <!-- Character encoding -->
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <script type="text/javascript" src="">
+  </script>
+</head>
+<body>
+<canvas id="canvas"> Your browser does not support HTML5 Canvas, please try opera, google chrome or firefox </canvas>
+</body>
+</html>
